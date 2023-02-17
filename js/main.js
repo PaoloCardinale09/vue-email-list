9fd84dbd71fb21e15cf416a1f0742d8ad4dd7494
PaoloCardinale09 <alfiozeronove@gmail.com>
2023-02-17 17:42:38 +0100

    init projext

---
+++ js/main.js
@@ -0,0 +1,23 @@
+for (let index = 0; index < 10; index++) {
+  axios
+    .get("https://flynn.boolean.careers/exercises/api/random/mail")
+    .then(function (response) {
+      console.log(response.data.response);
+    });
+}
+
+const { createApp } = Vue;
+
+createApp({
+  data() {
+    return {
+      message: "Hello Vue!",
+    };
+  },
+}).mount("#app");
+// Descrizione
+// Attraverso l'apposita API di Boolean
+// https://flynn.boolean.careers/exercises/api/random/mail
+// generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+// Bonus
+// Far comparire gli indirizzi email solamente quando sono stati tutti generati.
